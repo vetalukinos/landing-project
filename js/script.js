2d0e148524b701e80b605f33d86b5eef2e947936
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-04-30 16:17:35 +0300

    fix 25 lesson

--- js/script.js
+++ js/script.js
@@ -402,6 +402,106 @@ window.addEventListener('DOMContentLoaded', function() {
 
     calc(100);
 
+
+    //send-ajax=form
+    const sendForm = () => {
+
+        //Сообщения для пользователя
+        const errorMessage = 'Что-то пошло не так...',
+            loadMessage = document.getElementById('cube-loader'),
+            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+        const form = document.querySelectorAll('form'),
+            allInputs = document.querySelectorAll('form input');
+
+        //Создаем div с сообщением
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = 'font-size: 2 rem;';
+        //Изменение цвета для сообщения формы в модальном окне
+        if (document.getElementById('form3')) {
+            statusMessage.style.cssText = 'color: #fff;';
+        }
+
+        let checkInputByRussianWords = document.querySelectorAll('[name="user_name"], [name="user_message"]');
+        checkInputByRussianWords.forEach((elem) => {
+            elem.addEventListener('input', function() {
+                this.value = this.value.replace(/[^А-Яа-яЁё ]/, '');
+            })
+        });
+
+        //Проверяем инпуты на число
+        const checkInputByNumbers = document.querySelectorAll('[name="user_phone"]');
+        checkInputByNumbers.forEach((elem) => {
+            elem.addEventListener('input', function() {
+                this.value = this.value.replace(/[^0-9\+]/, '');
+            })
+        });
+
+        form.forEach((elem) => {
+            //Навешиваем на форму обработчик событий submit
+            elem.addEventListener('submit', (event) => {
+                allInputs.forEach((elem) => {
+                    elem.value = '';
+                });
+
+                event.preventDefault(); //запрещаем перезагрузку страницы
+                elem.appendChild(statusMessage); //помещаем на страницу statusMessage
+                statusMessage.textContent = '';
+                statusMessage.classList.add('sk-spinner-pulse');
+
+                //перед отправкой данных нужно их получить с помощью FormData
+                const formData = new FormData(elem); //получаем все данные из нашей формы
+                /*Получаем объект с нашими данными*/
+                let body = {};
+
+                formData.forEach((val, key) => {
+                    body[key] = val;
+                });
+
+                /*Отправляем и обрабатываем запрос на сервер*/
+                postData(body, () => { //функция outputData
+                    statusMessage.classList.remove('sk-spinner-pulse');
+                    statusMessage.textContent = successMessage;
+                }, (error) => { //функция errorData
+                    statusMessage.classList.remove('sk-spinner-pulse');
+                    statusMessage.textContent = errorMessage;
+                    console.log(error);
+                });
+            });
+        });
+
+        /*Функция запроса на сервер*/
+        const postData = (body, outputData, errorData) => {
+            //Пишем запрос к серверу (создаем объект экземпляра XMLHttpRequest)
+            const request = new XMLHttpRequest();
+
+            //Оповещаем пользователя о том, что данные ушли на сервер
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    outputData(); //вызываем функцию outputData
+                } else {
+                    errorData(request.status); //вызываем функцию errorData
+                }
+
+            });
+
+            //настраиваем соединение
+            request.open('POST', 'server.php'); //отправляем запрос на сервер через POST
+
+            //настраиваем заголовки
+            request.setRequestHeader('Content-Type', 'application/json');
+
+            //отправляем данные
+            request.send(JSON.stringify(body));
+        }
+
+    };
+
+    sendForm();
+
 });
 
 
